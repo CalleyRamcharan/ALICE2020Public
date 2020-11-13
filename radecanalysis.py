7cbfb1369d3a57876c53b639a41ae5fce6455903
CalleyRamcharan <72687135+CalleyRamcharan@users.noreply.github.com>
2020-11-13 16:36:44 +0200

    Added Comments

--- radecanalysis.py
+++ radecanalysis.py
@@ -2,8 +2,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#Output file of coordinates.py had to have minor manual changes made to allow it to be read into this code as an input file
+# Input file format should be of the form:
+# [ , ] [ , ] [ , ] - with entries of each array being floats
+# ^^ This is different to the output file from coordinates.py - so sorry :( we didn't have time to write to the file in a nice colummated format :(
+# find and replace should do the trick to help you out with reformatting the output file from coordinates.py, unless if you want to go edit
+# coordinates.py to let it write in a nice format, then by all means be my guest
+
+
+#read in input file
 radec = open("coordinates.txt", "r")
 
+#read entire file as one large string
 file = radec.read(-1)
 file = file.split(" ")
 
@@ -16,6 +27,7 @@ for i in range(0,len(file)):
     print(RA[i])
     dec[i] = float(arr[1][:arr[1].index("]")])
 
+#create polar plot of RA and dec values to see dispersion of cosmic rays    
 plt.polar(RA,dec,'ro',ms=2)
 plt.show()
 
